f07c9d23e5d13c08a7d641e5c3800b5c01cd0784
ricardo-chan <47405287+ricardo-chan@users.noreply.github.com>
2019-03-11 00:32:45 -0600

    Random datasets are now generated each time the code runs. Adapted plotting to account for new range for datasets.Multiple iterations implemented to test for rate of convergence.

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Single layer perceptron implementation in python.
 ### What I learned:
 
 * Simple implementation of perceptron using matplotlib as well as numpy libraries.
+* Generating random points to use as test data
+* Testing rate of convergence for the algorithm.
+
+If the multiple iterations for the algorithm are used, it is recommended to comment our the line where the final epoch is plotted.
+
+This code was based on [this project](https://github.com/bfaure/AI_Project_4/blob/master/Question_4/main.py).
+
+Used as an exercise to further comprehend concepts seen in [Learning From Data Course](http://book.caltech.edu/bookforum/index.php)
--- main.py
+++ main.py
@@ -3,6 +3,52 @@ import matplotlib, sys
 from matplotlib import pyplot as plt
 import numpy as np
 
+def classifyTrainingPoint(trainingPoints, line):
+	#	Checking to see which classification do the training points
+	#	fall under.
+	slope = line[0]
+	b = line[1]
+
+	for trainingPoint in trainingPoints:
+		x1_Given = trainingPoint[1]
+		x2_Given = trainingPoint[2]
+		x2_Calculated = (x1_Given * slope) + b
+		if x2_Calculated < x2_Given:
+			trainingPoint[3] = 1.0
+		else:
+			trainingPoint[3] = 0.0
+
+def genLine():
+	x1, y1 = np.random.uniform(-1, 1, 2)	#	First point
+	x2, y2 = np.random.uniform(-1, 1, 2)	#	Second point
+
+	#	Taking the form of y = mx + b... Calculating.
+	m = (y2 - y1) / (x2 - x1)
+	b = y1 - (m * x1)
+
+	#	Plotting line
+	#hor = np.linspace(-1,1)
+	#xs = [x1, x2]
+	#ys = [y1, y2]
+	#c1s = plt.scatter(xs,ys,s=40.0,c='g')
+	#plt.plot(hor,m*hor + b, label='Division')
+
+	y = [m, b]
+	return y
+
+def genData(trainingPoints = 10):
+	dataSet = []
+	for i in range(trainingPoints):
+		trainingPoint = [0.00, 0.00, 0.00, 0.00]
+		trainingPoint[0] = 1.00		# Bias
+		x1, x2 = np.random.uniform(-1, 1, 2)
+		trainingPoint[1] = x1		# x1
+		trainingPoint[2] = x2		# x2
+		trainingPoint[3] = 0.0		# y output initialized to 0
+		dataSet.append(trainingPoint)
+
+	return dataSet
+
 def plot(matrix, weights = None, title = "Prediction Matrix"):
 
 	if len(matrix[0])==3: # if 1D inputs, excluding bias and ys
@@ -12,9 +58,9 @@ def plot(matrix, weights = None, title = "Prediction Matrix"):
 		ax.set_ylabel("Classifications")
 
 		if weights!=None:
-			y_min=-0.1
+			y_min=-1.1
 			y_max=1.1
-			x_min=0.0
+			x_min=-1.1
 			x_max=1.1
 			y_res=0.001
 			x_res=0.001
@@ -45,8 +91,8 @@ def plot(matrix, weights = None, title = "Prediction Matrix"):
 
 		plt.xticks(np.arange(x_min,x_max,0.1))
 		plt.yticks(np.arange(y_min,y_max,0.1))
-		plt.xlim(0,1.05)
-		plt.ylim(-0.05,1.05)
+		plt.xlim(-1.05,1.05)
+		plt.ylim(-1.05,1.05)
 
 		c0s = plt.scatter(c0_data[0],c0_data[1],s=40.0,c='r',label='Class -1')
 		c1s = plt.scatter(c1_data[0],c1_data[1],s=40.0,c='b',label='Class 1')
@@ -62,7 +108,7 @@ def plot(matrix, weights = None, title = "Prediction Matrix"):
 		ax.set_ylabel("i2")
 
 		if weights!=None:
-			map_min=0.0
+			map_min=-1.1
 			map_max=1.1
 			y_res=0.001
 			x_res=0.001
@@ -90,10 +136,10 @@ def plot(matrix, weights = None, title = "Prediction Matrix"):
 				c0_data[0].append(cur_i1)
 				c0_data[1].append(cur_i2)
 
-		plt.xticks(np.arange(0.0,1.1,0.1))
-		plt.yticks(np.arange(0.0,1.1,0.1))
-		plt.xlim(0,1.05)
-		plt.ylim(0,1.05)
+		plt.xticks(np.arange(-1.1,1.1,0.1))
+		plt.yticks(np.arange(-1.1,1.1,0.1))
+		plt.xlim(-1.05,1.05)
+		plt.ylim(-1.05,1.05)
 
 		c0s = plt.scatter(c0_data[0],c0_data[1],s=40.0,c='r',label='Class -1')
 		c1s = plt.scatter(c1_data[0],c1_data[1],s=40.0,c='b',label='Class 1')
@@ -105,85 +151,93 @@ def plot(matrix, weights = None, title = "Prediction Matrix"):
 	print("Matrix dimensions not covered.")
 
 def predict(inputs, weights):
-    threshold = 0.0
-    total_activation = 0.0
+	threshold = 0.0
+	total_activation = 0.0
 
-    for input,weight in zip(inputs,weights):
-        total_activation += input*weight
-    return 1.0 if total_activation >= threshold else 0.0
+	for input,weight in zip(inputs,weights):
+		total_activation += input*weight
+	return 1.0 if total_activation >= threshold else 0.0
 
 #   Calculate prediction accuracy, providad inputs
 #   and associated weights...
 def accuracy(matrix, weights):
-    num_correct = 0.0
-    preds = []
+	num_correct = 0.0
+	preds = []
 
-    for i in range(len(matrix)):
-        # get predicted classification
-        pred = predict(matrix[i][:-1], weights)
-        preds.append(pred)
-        # check if prediction is accurate
-        if pred == matrix[i][-1]: num_correct += 1.0
+	for i in range(len(matrix)):
+		# get predicted classification
+		pred = predict(matrix[i][:-1], weights)
+		preds.append(pred)
+		# check if prediction is accurate
+		if pred == matrix[i][-1]: num_correct += 1.0
 
-    print("Predictions:", preds)
-    # return overall prediction accuracy
-    return num_correct/float(len(matrix))
+	print("Predictions:", preds)
+	# return overall prediction accuracy
+	return num_correct/float(len(matrix))
 
 #   Train the perceptron on the data found in the Matrix
 #   trained weights are returned at the end of the function
 def train_weights(matrix, weights, nb_epoch = 10, l_rate = 1.0,
-            do_plot = False, stop_early = True, verbose = True):
-    # iterate for the number of epochs requested
-    for epoch in range(nb_epoch):
-        # calculate the current accuracy
-        cur_acc = accuracy(matrix, weights)
-        # print out information
-        print("\nEpoch %d \nWeights: " %epoch, weights)
-        print("Accuracy: ", cur_acc)
+			do_plot = False, stop_early = True, verbose = True):
+	# iterate for the number of epochs requested
+	for epoch in range(nb_epoch):
+		# calculate the current accuracy
+		cur_acc = accuracy(matrix, weights)
+		# print out information
+		print("\nEpoch %d \nWeights: " %epoch, weights)
+		print("Accuracy: ", cur_acc)
 
-        # check if we are finished training
-        if cur_acc == 1.0 and stop_early: break
+		# check if we are finished training
+		if cur_acc == 1.0 and stop_early: break
 
-        # check if we should plot the current results
-        if do_plot: plot(matrix, weights, title = "Epoch %d" %epoch)
+		# check if we should plot the current results
+		if do_plot: plot(matrix, weights, title = "Epoch %d" %epoch)
 
-        for i in range(len(matrix)):
-            # calculate Prediction
-            prediction = predict(matrix[i][:-1], weights)
-            # calculate error
-            error = matrix[i][-1] - prediction
+		for i in range(len(matrix)):
+			# calculate Prediction
+			prediction = predict(matrix[i][:-1], weights)
+			# calculate error
+			error = matrix[i][-1] - prediction
 
-            if verbose:
-                print("Training on data at index %d..." %i)
+			#if error != 0:
+			if verbose:
+				print("Training on data at index %d..." %i)
 
-            for j in range(len(weights)):
-                if verbose:
-                    sys.stdout.write("\tWeight[%d]: %0.5f --> " %(j,weights[j]))
+			for j in range(len(weights)):
+				if verbose:
+					sys.stdout.write("\tWeight[%d]: %0.5f --> " %(j,weights[j]))
 
-                weights[j] = weights[j] + (l_rate * error * matrix[i][j])
+				weights[j] = weights[j] + (l_rate * error * matrix[i][j])
 
-                if verbose:
-                    sys.stdout.write("%0.5f\n" %weights[j])
+				if verbose:
+					sys.stdout.write("%0.5f\n" %weights[j])
 
-    # plot out the final results
-    plot(matrix, weights, title = "Final Epoch")
-    return weights
+	# plot out the final results
+	plot(matrix, weights, title = "Final Epoch")
+	return epoch
 
 def main():
+	#	This is in case the experiment needs to be run multiple times
+	loops = range(1)
+	iterations = len(loops)
+	sum = 0
 
-    #            Bias  x1    x2    y
-    data = [    [1.00, 0.08, 0.72, 1.0],
-                [1.00, 0.10, 1.00, 0.0],
-                [1.00, 0.26, 0.58, 1.0],
-                [1.00, 0.35, 0.95, 0.0],
-                [1.00, 0.45, 0.15, 1.0],
-                [1.00, 0.60, 0.30, 1.0],
-                [1.00, 0.70, 0.65, 0.0],
-                [1.00, 0.92, 0.45, 0.0]]
-    weights =   [0.20, 1.00,-1.00]
+	for test in loops:
+		#	Generating data
+		data = genData(100)
+		classifLine = genLine()
+		classifyTrainingPoint(data, classifLine)
+		weights	= [0.00, 0.00, 0.00]
 
-    train_weights(data, weights = weights, nb_epoch = 10,
-        l_rate = 1.0, do_plot = True, stop_early = True)
+		#	Executing algorithm
+		iteration = train_weights(data, weights = weights, nb_epoch = 10000, l_rate = 1.0,
+					do_plot = False, stop_early = True, verbose = False)
+
+		sum += iteration
+
+	avg = sum / iterations
+
+	print("It took %d iterations on average" %avg)
 
 if __name__ == '__main__':
-    main()
+	main()
